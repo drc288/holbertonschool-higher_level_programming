084d5c0bddd596667ebe3e0628ef6931c7abdb16
Drose <davidroserocalle@gmail.com>
2019-09-12 21:46:53 +0000

    Not executable when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-import hidden_4
-for i in dir(hidden_4):
-    if "__" in i:
-        continue
-    else:
-        print(i)
+if __name__ == "__main":
+    import hidden_4
+    for i in dir(hidden_4):
+        if "__" in i:
+            continue
+        else:
+            print(i)
