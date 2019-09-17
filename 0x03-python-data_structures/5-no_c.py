6e7f37fa96e5b1fa2e2034042ecd0ed8abb8418b
Drose <davidroserocalle@gmail.com>
2019-09-17 18:42:51 +0000

    is NULL

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     n = 0
-    if my_string is None:
-        return (None)
+    if my_string == "":
+        return (my_string)
     for i in my_string:
         if ord(i) == 99 or ord(i) == 67:
             my_string = my_string[0:n] + my_string[n+1:]
