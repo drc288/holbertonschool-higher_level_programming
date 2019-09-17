a3ceb21f6b33b0060b0106abe984e6c04cb9d295
Drose <davidroserocalle@gmail.com>
2019-09-17 21:13:57 +0000

    condition if 0 in the initial operation

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,13 +5,13 @@ def add_tuple(tuple_a=(), tuple_b=()):
     suma = 0
     suma2 = 0
     tupla=()
-    if len(b) < 2:
+    if len(b) == 0:
+        tupla = (a[0], a[1])
+        return(tupla)
+    elif len(b) < 2:
         b = b + (0,)
         suma = a[0] + b[0]
         tupla = (suma, a[1])
-        return (tupla)
-    elif len(b) == 0:
-        tupla = (a[0], a[1])
         return (tupla)
     tupla = (a[0] + b[0], a[1] + b[1])
     return (tupla)
