e1db22449afaa68fa62dafdb08dbf2bb9be2eaea
Drose <davidroserocalle@gmail.com>
2019-09-17 21:20:22 +0000

    tuplas

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,14 +4,15 @@ def add_tuple(tuple_a=(), tuple_b=()):
     b = tuple_b
     suma = 0
     suma2 = 0
-    tupla=()
+    tupla = ()
     if len(b) == 0:
-        tupla = (a[0], a[1])
+        b = (0,0)
+        tupla = (a[0] + b[0], a[1] + b[1])
         return(tupla)
     elif len(b) < 2:
         b = b + (0,)
         suma = a[0] + b[0]
-        tupla = (suma, a[1])
+        tupla = (suma, a[1] + b[1])
         return (tupla)
     tupla = (a[0] + b[0], a[1] + b[1])
     return (tupla)
