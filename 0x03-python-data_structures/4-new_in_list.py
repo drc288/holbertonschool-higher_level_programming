11b472c65c05133372cbd08777487c427367302a
Drose <davidroserocalle@gmail.com>
2019-09-17 17:18:49 +0000

    condition > to >=

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     l1 = my_list.copy()
     if idx < 0:
         return (l1)
-    if idx > len(my_list):
+    if idx >= len(my_list):
         return (l1)
     l1[idx] = element
     return (l1)
