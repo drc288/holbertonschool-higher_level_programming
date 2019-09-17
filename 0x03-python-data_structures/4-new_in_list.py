0e249d554106fa036b85244774d1f3dcf70302a2
Drose <davidroserocalle@gmail.com>
2019-09-17 17:07:09 +0000

    fix 0 to casting and add pint 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    l1 = my_list.copy()
+    if idx < 0:
+        return (l1)
+    if idx > len(my_list):
+        return (l1)
+    l1[idx] = element
+    return (l1)
