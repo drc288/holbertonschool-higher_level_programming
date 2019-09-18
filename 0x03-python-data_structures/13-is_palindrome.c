5564c2092f5eeef4c6d8fdbefde887d18c3e2a1e
Drose <davidroserocalle@gmail.com>
2019-09-18 02:40:52 +0000

    import stdio and fix betty in is palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -8,28 +8,28 @@
  */
 int is_palindrome(listint_t **head)
 {
-    int nodes = 0, i = 0, j = 0;
-    int li[200];
-    listint_t *tmp;
+	int nodes = 0, i = 0, j = 0;
+	int li[200];
+	listint_t *tmp;
 
-    tmp = *head;
-    if (head == NULL || *head == NULL || (*head)->next == NULL )
-        return (1);
+	tmp = *head;
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
 
-    while (tmp != NULL)
-    {
-        li[nodes] = tmp->n;
-        tmp = tmp->next;
-        nodes++;
-    }
+	while (tmp != NULL)
+	{
+		li[nodes] = tmp->n;
+		tmp = tmp->next;
+		nodes++;
+	}
 
-    i = nodes - 1;
-    while (i >= 0 && j <= i)
-    {
-        if (li[i] != li[j])
-            return (0);
-        i--;
-        j++;
-    }
-    return (1);
+	i = nodes - 1;
+	while (i >= 0 && j <= i)
+	{
+		if (li[i] != li[j])
+			return (0);
+		i--;
+		j++;
+	}
+	return (1);
 }
