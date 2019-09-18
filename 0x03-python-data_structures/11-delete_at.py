120f42d8fc8b549c179f040d81b3671408d7ef1c
Drose <33203315+drc288@users.noreply.github.com>
2019-09-17 20:23:58 -0500

    Add point 11, delete idx in list

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    lent = len(my_list)
+    if idx < 0:
+        return my_list
+    elif idx >= lent:
+        return my_list
+    my_list.remove(idx)
+    return my_list
