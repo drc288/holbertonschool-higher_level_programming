064d9c2de1b11b1498a10f3beaa96f2e357f14e5
Drose <davidroserocalle@gmail.com>
2019-09-19 18:18:01 +0000

    change the logic

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    f = lambda x: replace if x == search else x
-    for i in range(0, len(my_list)):
-        new = [f(x) for x in my_list]
-    return new
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return new_list
