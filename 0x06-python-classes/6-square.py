1170b9b19f41488d55198a9de2aca389365fb98f
Drose <davidroserocalle@gmail.com>
2019-10-02 02:05:06 +0000

    add point 6

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+class Square:
+    __size = None
+    __position = None
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.__position = position
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        for newline in range(self.__position[1]):
+            print()
+        for i in range(self.__size):
+            for spaces in range(self.__position[0]):
+                print(" ", end='')
+            for j in range(self.__size):
+                print("#", end="")
+            print()
+
+    """For Position"""
+    @property
+    def position(self):
+        return self.__position
+    
+    @position.setter
+    def position(self, value):
+        if type(value) is not tuple or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif type(value[0]) is not int or type(value[1]) is not int:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = position
+
+
+    """For Size"""
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
