4bdaeb7c9b004abee3e3d1ca72895cef29dc4bf4
Drose <davidroserocalle@gmail.com>
2019-10-03 19:41:14 +0000

    add point 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" 
+    add_integer - add a to b
+    >>> add_integer(5, 5)
+    10
+"""
+def add_integer(a, b=98):
+    if type(a) is not float and type(a) is not int:
+        raise TypeError("a must be an integer")
+    elif type(b) is not float and type(b) is not int:
+        raise TypeError("b must be an integer")
+    else:
+        """ 
+        Returns a + b
+        """
+        return int(a) + int(b)
