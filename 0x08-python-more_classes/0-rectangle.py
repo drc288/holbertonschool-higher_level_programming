fb7d4250131741a74276b4d3768e37036acbf173
Drose <davidroserocalle@gmail.com>
2019-10-07 15:34:04 +0000

    Empty class and readme file

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    Rectangle - Empty class of rectangle
+    >>> r = Rectangle()
+"""
+
+
+class Rectangle:
+    """
+    Rectangle: Empty class, type Rectangle
+    """
+    pass
