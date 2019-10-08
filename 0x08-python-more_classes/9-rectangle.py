fe739f74ea72b87bb4c28e522c7ad0a4107dfa37
Drose <davidroserocalle@gmail.com>
2019-10-08 01:06:08 +0000

    Add point 9

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -76,7 +76,7 @@ class Rectangle:
     """ Class method """
     @classmethod
     def square(cls, size=0):
-        return cls(size, size)
+        return Rectangle(size, size)
 
     """ For width """
     @property
