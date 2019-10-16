abff6eb56ffbd3be2e603a2ad0f25d0175bbd9af
Drose <davidroserocalle@gmail.com>
2019-10-16 16:44:20 +0000

    add point 2

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    """ read_lines - read lines with nb_lines """
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read())
+        else:
+            for i in range(nb_lines):
+                print(f.readline(), end="")
