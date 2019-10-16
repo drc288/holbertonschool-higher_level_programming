59bf28058f86eac20bb4a9ae9945b80911af4133
Drose <davidroserocalle@gmail.com>
2019-10-16 05:33:54 +0000

    fix

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 """ inherits_from - identify if ibj are subclass of a_class
 """
+
+
 def inherits_from(obj, a_class):
+    """ issubclass - verify if subclass """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return True
     else:
