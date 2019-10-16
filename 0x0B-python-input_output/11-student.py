d62ef0aae3fb633da6d4b2182d0934879f9d14fe
Drose <davidroserocalle@gmail.com>
2019-10-16 21:48:45 +0000

    add point 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Class Student - define class
+"""
+
+
+class Student:
+    """ __init__ - init fname, lname and age """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+    """ to_json - Convert to json """
+    def to_json(self):
+        return self.__dict__
