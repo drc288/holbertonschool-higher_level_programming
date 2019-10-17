b983976bbe4f3bbc1a53d69e8b6316c28ea0db80
Drose <davidroserocalle@gmail.com>
2019-10-17 01:42:45 +0000

    add point 13

---
+++ 0x0B-python-input_output/13-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" Class Student - define class
+"""
+
+
+class Student:
+    """ __init__ - init fname, lname and age """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ to_json - Convert to json """
+        if attrs is None:
+            return self.__dict__
+        tmp_dict = dict()
+        for key in attrs:
+            if key in self.__dict__.keys():
+                tmp_dict[key] = self.__dict__[key]
+        return tmp_dict
+    
+    def reload_from_json(self, json):
+        """ reload_from_json - reload the json """
+        if len(json) != 0:
+            self.first_name = json["first_name"]
+            self.last_name = json["last_name"]
+            self.age = json["age"]
