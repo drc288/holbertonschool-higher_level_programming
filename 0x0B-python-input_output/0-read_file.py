a6793d7859558edd0d9ccad71562d11afeb0f699
Drose <davidroserocalle@gmail.com>
2019-10-17 00:54:23 +0000

    Hotfix

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 def read_file(filename=""):
     """ read_file - read the file filename """
     with open(filename, encoding="utf-8") as f:
-        print(f.read())
+        print(f.read(), end="")
