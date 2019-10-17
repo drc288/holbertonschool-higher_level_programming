82153e7e2d96e371c1da91785c6afd4d58cd9a7e
Drose <davidroserocalle@gmail.com>
2019-10-17 01:21:02 +0000

    add more descriptions

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+""" Load a json from file """
 import json
 
 
 def load_from_json_file(filename):
     """ load_from_json_file - get the json and convert to python obj """
-    with open(filename) as f:
+    with open(filename, encoding="utf-8") as f:
         a = json.loads(f.read())
     return a
