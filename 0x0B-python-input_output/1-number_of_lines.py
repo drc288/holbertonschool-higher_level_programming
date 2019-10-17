c9517ce7c08da037eceb3770effa578d51481eea
Drose <davidroserocalle@gmail.com>
2019-10-17 00:57:42 +0000

    Hotfix

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -3,6 +3,5 @@ def number_of_lines(filename=""):
     """ number_of_lines - return the number of lines """
     count = 0
     with open(filename) as f:
-        for line in f:
-            count += 1
-    return count - 1
+        count = f.readlines()
+    return count
