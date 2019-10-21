5bb91275c668d5e4c0b8c83cd75e29c281d3f462
Drose <davidroserocalle@gmail.com>
2019-10-21 20:48:33 +0000

    add files

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -22,7 +22,9 @@ class Rectangle(Base):
         super().__init__(id)
 
     def __str__(self):
-        """ __str__ - return the rectangle class str """
+        """
+        __str__ - return the rectangle class str
+        """
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x,
                                                        self.y, self.width,
                                                        self.height)
@@ -48,23 +50,37 @@ class Rectangle(Base):
                 print(" " * space, end="")
             print("#" * self.width)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """
-        update - update the Rectangle
+        update - update the Rectangle, 
         """
         # Get the len of args
         len_args = len(args)
-        for i, data in enumerate(args):
-            if i == 0:
-                self.id = data
-            if i == 1:
-                self.width = data
-            if i == 2:
-                self.height = data
-            if i == 3:
-                self.x = data
-            if i == 4:
-                self.y = data
+        len_kwargs = len(kwargs)
+        if len_kwargs != 0 and len_args == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "height":
+                    self.height = value
+                if key == "width":
+                    self.width = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+        if len_args != 0:
+            for i, data in enumerate(args):
+                if i == 0:
+                    self.id = data
+                if i == 1:
+                    self.width = data
+                if i == 2:
+                    self.height = data
+                if i == 3:
+                    self.x = data
+                if i == 4:
+                    self.y = data
 
     @property
     def width(self):
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+from models.rectangle import Rectangle
+"""
+Square - create a object square
+"""
+
+
+class Square(Rectangle):
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        Init the constructor - super().__init__(size, size, x, y)
+        call the super class
+        """
+        super().__init__(size, size, x, y)
+    
+    def __str__(self):
+        """
+        __str__ - return the rectangle class str
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.size)
+    
+    def update(self, *args, **kwargs):
+        """
+        update - update the Rectangle, 
+        """
+        # Get the len of args
+        len_args = len(args)
+        len_kwargs = len(kwargs)
+        if len_kwargs != 0 and len_args == 0:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
+        if len_args != 0:
+            for i, data in enumerate(args):
+                if i == 0:
+                    self.id = data
+                if i == 1:
+                    self.size = data
+                if i == 2:
+                    self.x = data
+                if i == 3:
+                    self.y = data
+
+    @property
+    def size(self):
+        """
+        size - method getter
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        size - method setter
+        """
+        self.width = value
+        self.height = value
