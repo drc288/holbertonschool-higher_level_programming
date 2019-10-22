ab33c52950d2c4134f7f02ef7c3661da51fddba3
Drose <davidroserocalle@gmail.com>
2019-10-22 18:55:38 +0000

    fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -137,7 +137,7 @@ class Rectangle(Base):
         """ x - Setter for x, validate the data """
         if not isinstance(value, int):
             raise TypeError("x must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("x must be >= 0")
         else:
             self.__x = value
@@ -154,7 +154,7 @@ class Rectangle(Base):
         """ y - Setter for y, validate the data"""
         if not isinstance(value, int):
             raise TypeError("y must be an integer")
-        if value < 0:
+        if value <= 0:
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
