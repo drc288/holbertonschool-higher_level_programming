7e6a257d6dc3d18d4ff9418dcab468b1936df3c6
Drose <davidroserocalle@gmail.com>
2019-10-22 19:31:25 +0000

    fix

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,7 +26,8 @@ class Base:
         """
         if list_dictionaries is None:
             return []
-        return json.dumps(list_dictionaries)
+        else:
+            return json.dumps(list_dictionaries)
 
     @staticmethod
     def from_json_string(json_string):
