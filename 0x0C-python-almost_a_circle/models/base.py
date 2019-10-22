aeba42913a483020541c251ff929fb59d3f8eba0
Drose <davidroserocalle@gmail.com>
2019-10-22 18:43:02 +0000

    add coments

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -6,9 +6,9 @@ Class Base - init the proces id
 
 
 class Base:
+    """ Base """
     # Create private attribute
     __nb_objects = 0
-
     def __init__(self, id=None):
         """
         init - init the constructor
