77d396fdbfa20d0b9b019a47df5819b6ba4fafc3
Drose <davidroserocalle@gmail.com>
2019-11-25 17:54:40 +0000

    add points

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,2 @@
+-- Create the database hbtn_0c_0 if not exists
+CREATE DATABASE if not EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- First : Create DATABE if not exists
+-- Second : Create the table and add data types
+CREATE DATABASE if not EXISTS hbtn_0c_0;
+
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+)
