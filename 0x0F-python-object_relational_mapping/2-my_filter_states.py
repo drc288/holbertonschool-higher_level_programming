684fd359b09f69f8ca233dc0961ab98476627010
David Rosero C <davidroserocalle@gmail.com>
2019-12-14 23:37:38 +0000

    edit file

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 import sys
 import MySQLdb
-
+"""
+This script print the data with the state_n
+"""
 if __name__ == "__main__":
-    if len(sys.argv)  == 5:
+    if len(sys.argv) == 5:
         user = sys.argv[1]
         pas = sys.argv[2]
         db = sys.argv[3]
@@ -15,10 +17,10 @@ if __name__ == "__main__":
                              db=db)
 
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE name=\'{:s}\'".format(state_n))
+        cur.execute("SELECT * FROM states "
+                    "WHERE name=\'{:s}\' "
+                    "ORDER BY states.id ASC".format(state_n))
         rows = cur.fetchall()
 
         for row in rows:
             print(row)
-
-        cur.close()
