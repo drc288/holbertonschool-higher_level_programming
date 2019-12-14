22a5fa91abe41d56500ef05c5801131e425624a6
David Rosero C <davidroserocalle@gmail.com>
2019-12-14 23:15:11 +0000

    edit file and add the mysql LIKE

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 import sys
 import MySQLdb
+"""
+This script get all data where init wiht n
+"""
 
 if __name__ == "__main__":
     if len(sys.argv) == 4:
+        # Get the data
         user = sys.argv[1]
         pas = sys.argv[2]
         db = sys.argv[3]
 
+        # Create the conection
         db = MySQLdb.connect(host='localhost',
                              user=user,
                              passwd=pas,
                              db=db)
 
+        # Get new instance
         cur = db.cursor()
-        cur.execute("SELECT * FROM states WHERE (name REGEXP '^N')")
+        # Execute the query
+        # PSDT: LIKE using and search the data with init n or N
+        cur.execute("SELECT * FROM states WHERE name LIKE 'n%'")
         rows = cur.fetchall()
 
+        # Print the data
         for row in rows:
             print(row)
-
-        cur.close()
 
