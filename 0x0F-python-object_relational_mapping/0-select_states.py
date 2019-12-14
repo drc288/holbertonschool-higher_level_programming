93411329b09609f9ce4ab18f559670b7b52d2874
David Rosero C <davidroserocalle@gmail.com>
2019-12-14 23:00:36 +0000

    fix problem and understand mysql

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 import sys
 import MySQLdb
+"""
+This script connect to a database and print te obj
+"""
+
 
 if __name__ == "__main__":
+    # Get the data
     user = sys.argv[1]
     pas = sys.argv[2]
     db = sys.argv[3]
 
+    # Create the connection
     db = MySQLdb.connect(host='localhost',
                          user=user,
                          passwd=pas,
                          db=db)
 
+    # Get the cursor, this is to create difrents
+    # working env
     cur = db.cursor()
+    # Command to execute
     cur.execute("SELECT * FROM states")
 
+    # Get the rdata of the query and print
     rows = cur.fetchall()
+    print(rows)
     for row in rows:
         print(row)
 
