15f7792c2d45bede0f173355c976ab0094bdde81
David Rosero C <davidroserocalle@gmail.com>
2019-12-15 19:11:23 +0000

    add point 11 and jmm

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+import sys
+import sqlalchemy as db
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+"""
+
+"""
+
+
+if __name__ == "__main__":
+    # Get the data
+    user = sys.argv[1]
+    pswd = sys.argv[2]
+    def_db = sys.argv[3]
+
+    # Create the connection
+    eng = db.create_engine(
+        'mysql+mysqldb://{:s}:{:s}@localhost/{:s}'
+        .format(user, pswd, def_db)
+    )
+
+    # Create the metadata of the connection eng
+    #---------CREATE-SESSION------------------
+    Base.metadata.create_all(eng)
+    new_session = sessionmaker(bind=eng)
+    Session = new_session()
+    #------------------------------------------
+
+    # Create data and save
+    new_data = State(name = 'Louisiana')
+    Session.add(new_data)
+    Session.commit()
+
+    print(new_data.id)
+
