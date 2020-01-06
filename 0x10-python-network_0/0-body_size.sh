69412cd4622c27cfe9ea88943762942829203317
David Rosero C <davidroserocalle@gmail.com>
2020-01-06 18:18:17 +0000

    update

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 # This script get the size of a header and printing, using curl
-# #############################################################
-# #            get the content lnght of argv[0]               #
-# #############################################################
-# Get the curl of the URL, get the data of content length
-# and print the second line
 curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}'
