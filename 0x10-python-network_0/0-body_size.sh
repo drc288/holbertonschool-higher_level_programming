d3082e347a0d7ada8377d47a18a6e3da4f90aa1c
David Rosero C <davidroserocalle@gmail.com>
2020-01-06 17:50:01 +0000

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 # This script get the size of a header and printing, using curl
-if [ $# == 1 ]; then
-	curl -so /dev/null "$1" -w '%{size_request}\n'
-fi
+curl -so /dev/null "$1" -w '%{size_request}\n'
