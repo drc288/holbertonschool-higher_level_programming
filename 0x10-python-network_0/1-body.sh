3f13f03f4ecec6c5e0d6f7fb2471f77fc531f086
David Rosero C <davidroserocalle@gmail.com>
2020-01-06 19:25:51 +0000

    add point

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 # This script get the head if the code status is 200
-STATUS=$(curl -o /dev/null -s -w "%{http_code}" "$1")
-if [ "$STATUS" -eq 200 ]; then
-	curl -o - "$1"
-fi
+curl -Lsf "$1"
