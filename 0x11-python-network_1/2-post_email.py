5492928b4871c709473a6684faf2df1305384a53
David Rosero C <davidroserocalle@gmail.com>
2020-01-13 18:24:57 +0000

    add point

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import urllib.parse
 import urllib.request
 import sys
 
-value = {'email': sys.argv[2]}
 
 if __name__ == "__main__":
+    value = {'email': sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode("ascii")
     req = urllib.request.Request(sys.argv[1], data)
