65aa8e5bc211dc7936766653dd87a5f4ccf5b506
David Rosero C <davidroserocalle@gmail.com>
2020-01-13 17:39:01 +0000

    add comments

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" Point number 1
+"""
 import urllib.request
 from sys import argv
 
-hbtn_url = argv[1]
 
+hbtn_url = argv[1]
 if __name__ == "__main__":
     with urllib.request.urlopen(hbtn_url) as res:
         print(res.headers.get("X-Request-Id"))
