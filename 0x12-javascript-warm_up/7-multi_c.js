176562d45b57649449a2296850c91a681c4ca16d
David Rosero C <davidroserocalle@gmail.com>
2020-02-04 18:44:22 +0000

    update

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+// print C is fun any times in argv
+const process = require('process');
+
+const argv = process.argv;
+const argc = argv.length;
+
+if (argc === 2) {
+  console.log('Missing number of occurrences');
+} else if (argc >= 3) {
+  const num = parseInt(argv[2]);
+  if (isNaN(num)) {
+    console.log('Missing number of occurrences');
+  } else {
+    for (let i = 0; i < num; i++) {
+      console.log("C is fun");
+    }
+  }
+}
