f386220e19af739a5cae379a6c32141a15025e5f
David Rosero C <davidroserocalle@gmail.com>
2020-02-04 18:58:48 +0000

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,16 +4,12 @@ const process = require('process');
 
 const argv = process.argv;
 const argc = argv.length;
+const num = parseInt(argv[2]);
 
-if (argc === 2) {
+if (isNaN(num)) {
   console.log('Missing number of occurrences');
-} else if (argc >= 3) {
-  const num = parseInt(argv[2]);
-  if (isNaN(num)) {
-    console.log('Missing number of occurrences');
-  } else {
-    for (let i = 0; i < num; i++) {
-      console.log("C is fun");
-    }
+} else {
+  for (let i = 0; i < num; i++) {
+    console.log('C is fun');
   }
 }
