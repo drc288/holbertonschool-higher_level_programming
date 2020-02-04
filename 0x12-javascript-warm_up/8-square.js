f2053c28534e59569b15aa8576aae34e35c6dfbc
David Rosero C <davidroserocalle@gmail.com>
2020-02-04 20:58:06 +0000

    semicolon fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -5,10 +5,9 @@ const argv = process.argv;
 const num = parseInt(argv[2]);
 
 if (isNaN(num)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let i = 0; i < num; i++) {
     console.log('x'.repeat(num));
   }
 }
-
