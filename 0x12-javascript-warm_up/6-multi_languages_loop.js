811cef8b01247c57cc81a9d1aac6fc6642a3830b
David Rosero C <davidroserocalle@gmail.com>
2020-02-04 18:26:57 +0000

    update

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Iterate in array
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
