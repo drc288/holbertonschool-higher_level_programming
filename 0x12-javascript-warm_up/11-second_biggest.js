20e0500bca6284c2cbe9d91ca6dfe1f2f4d447d6
David Rosero C <davidroserocalle@gmail.com>
2020-02-04 22:28:22 +0000

    continue in the home

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const process = require('process');
+
+const argv = process.argv;
+const argc = argv.length;
+
+if (argc === 2) {
+  console.log(0);
+} else if (argc === 3) {
+  console.log(0);
+} else {
+  for (let i = 3; i < argc; i++) {
+    
+  }
+}
