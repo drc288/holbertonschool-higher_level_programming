90d435a521245a4380494fd1772697f916e4d18c
David Rosero C <davidroserocalle@gmail.com>
2020-02-05 19:27:22 -0500

    update file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,19 @@ const process = require('process');
 
 const argv = process.argv;
 const argc = argv.length;
+let newArray = [];
 
 if (argc === 2) {
   console.log(0);
 } else if (argc === 3) {
   console.log(0);
 } else {
-  for (let i = 3; i < argc; i++) {
-    
+  for (let i = 2; i < argc; i++) {
+    newArray.push(argv[i]);
   }
 }
+
+const unique = new Set(newArray);
+newArray = [...unique].sort();
+const lenArray = newArray.length;
+console.log(newArray[lenArray - 2]);
