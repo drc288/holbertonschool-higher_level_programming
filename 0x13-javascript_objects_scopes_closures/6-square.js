04d0547247e16e17fe278889ffed8e6211002250
David Rosero C <davidroserocalle@gmail.com>
2020-02-10 15:45:14 -0500

    Update file, using the semicolon to recreate the print squere but changing the Char

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const Sq = require('./5-square');
+
+class Square extends Sq {
+  charPrint (c = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+}
+
+module.exports = Square;
