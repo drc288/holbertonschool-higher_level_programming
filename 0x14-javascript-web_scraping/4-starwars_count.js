a1a09af562d7a21abe91cc23d8e24b13142e7962
David Rosero C <davidroserocalle@gmail.com>
2020-02-11 21:37:07 +0000

    Fixing

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,16 +4,16 @@ const process = require('process');
 
 request(process.argv[2], (err, req, body) => {
   if (err) { console.log(err); }
-  let count = 0;
+  let countList = [];
   const films = JSON.parse(body).results;
 
   for (const film of films) {
     const characters = film.characters;
     for (const character of characters) {
       if (character.includes('18')) {
-        count += 1;
+        countList.push(character);
       }
     }
   }
-  console.log(count);
+  console.log(countList.length);
 });
